55b4dac984aaf5f35daa4ccd7b6b3d41d2ad357f
kishore <kishoresukant@gmail.com>
2021-03-19 19:41:46 +0530

    as

--- app.js
+++ app.js
@@ -32,12 +32,6 @@ connect.then(()=>{
 })
 mongoose.set("useCreateIndex",true);
 
-if (process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(--dirname,'client','build','index.html'));
-  })
-}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
